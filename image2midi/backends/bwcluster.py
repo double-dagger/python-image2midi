1b27b29d1aa6d96e27c93486404395af30c5dfd2
JC <srnci@xps9380.localdomain>
2021-01-29 21:37:40 +0100

    Sobel edge detection

--- image2midi/backends/bwcluster.py
+++ image2midi/backends/bwcluster.py
@@ -31,9 +31,17 @@ class Image(image2midi.backends.common.ClusterImage):
         self.track = parent
         self._im = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         self.init_image()
-        self._im2 = copy.copy(self._im)
-        self._im2 = cv2.blur(self._im2, (5,5))
-        self._im2 = cv2.Canny(self._im2,10,100)
+
+        # Apply blur and sobel edge detection for _im2
+        blur = cv2.GaussianBlur(self._im, (5,5), 0)
+        scale = 1
+        delta = 0
+        grad_x = cv2.Sobel(blur, cv2.CV_16S, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+        grad_y = cv2.Sobel(blur, cv2.CV_16S, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+        abs_grad_x = cv2.convertScaleAbs(grad_x)
+        abs_grad_y = cv2.convertScaleAbs(grad_y)
+        grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+        self._im2 = grad
 
     def get_cluster(self):
         return self.cluster_class(
